b8d3a6f9b5781babe002080d00f92bde27c65432
Sergei Dyshel <qyron.private@gmail.com>
2023-12-29 17:44:03 +0530

    +

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+};
