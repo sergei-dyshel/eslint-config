4fa000f2c63f383aec25f0495df03b5e5639c111
Sergei Dyshel <sergei@dyshel.com>
2024-05-26 09:05:16 +0300

    +

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginOnlyWarn from "eslint-plugin-only-warn";
+
 const off = "off";
 const never = "never";
 const warn = "warn";
 const always = "always";
 
-/**
- * @type {import("eslint").Linter.Config}
- */
-module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: warn,
+    },
+    rules: {
+      // handled by tsc
+      "@typescript-eslint/no-unused-vars": off,
 
-  rules: {
-    // handled by tsc
-    "@typescript-eslint/no-unused-vars": off,
-    "@typescript-eslint/no-namespace": off,
+      "@typescript-eslint/no-namespace": off,
+    },
   },
-};
+);
